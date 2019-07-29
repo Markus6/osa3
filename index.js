d930ce491ad07a4168b9378d5fc257f67c743c59
Markus Makela <markus.makela@tuni.fi>
2019-07-29 23:59:11 +0300

    puhelinluettelo backend step8

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan')
 
 app.use(bodyParser.json());
+//app.use(morgan('tiny'))
+morgan.token('postbody', function (req, res) { 
+    if (req.method == 'POST') {
+        return JSON.stringify(req.body);
+    }
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postbody'));
 
 let persons = [
     {
