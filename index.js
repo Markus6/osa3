ba2a7469aee1f25b4fe7f1e1902081af0405fc02
Markus Makela <markus.makela@tuni.fi>
2019-07-29 22:14:03 +0300

    puhelinluettelo backend step2

--- index.js
+++ index.js
@@ -3,28 +3,24 @@ const app = express();
 
 let persons = [
     {
-        "persons": [
-            {
-                "name": "Arto Hellas",
-                "number": "040-123456",
-                "id": 1
-            },
-            {
-                "name": "Ada Lovelace",
-                "number": "39-44-5323523",
-                "id": 2
-            },
-            {
-                "name": "Dan Abramov",
-                "number": "12-43-234345",
-                "id": 3
-            },
-            {
-                "name": "Mary Poppendieck",
-                "number": "39-23-6423122",
-                "id": 4
-            }
-        ]
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
     }
 ]
 
@@ -34,6 +30,11 @@ app.get('/', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
     res.json(persons);
+})
+
+app.get('/info', (req, res) => {
+    const moment = new Date();
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>${moment}`)
 })
 
 const PORT = 3001
