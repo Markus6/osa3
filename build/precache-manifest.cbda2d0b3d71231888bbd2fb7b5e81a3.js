8681ab013a30f0ebf302f8e0fe9ad08b81badc9f
Markus Makela <markus.makela@tuni.fi>
2019-08-01 13:38:59 +0300

    uibuild

---
+++ build/precache-manifest.cbda2d0b3d71231888bbd2fb7b5e81a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5684da8727a15a8c15cb2c9204bad5f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14f5f6460147fd09b816",
+    "url": "/static/css/main.84513904.chunk.css"
+  },
+  {
+    "revision": "bc120ca277fc438795d3",
+    "url": "/static/js/2.dbf5eb8f.chunk.js"
+  },
+  {
+    "revision": "14f5f6460147fd09b816",
+    "url": "/static/js/main.9d0462b0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
