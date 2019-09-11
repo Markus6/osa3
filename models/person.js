7a254dd7e45e91cdffeec3901df2528c27b8decf
Markus Makela <markus.makela@tuni.fi>
2019-09-11 11:23:35 +0300

    backend, puhelinluettelo ja tietokanta, step8

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 mongoose.set('useFindAndModify', false);
 
@@ -15,9 +16,17 @@ mongoose.connect(url, { useNewUrlParser: true })
 });
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+        type: String,
+        unique: true,
+        minlength: 3
+    },
+    number: {
+        type: String,
+        minlength: 8
+    }
 });
+personSchema.plugin(uniqueValidator);
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
